027c0824ce15735fcc592e7ff5b475398bbe33be
niriksha99 <30465025+niriksha99@users.noreply.github.com>
2018-09-14 11:13:04 +1000

    added de_activate trigger functionality

--- src/niriksha/Boulder.java
+++ src/niriksha/Boulder.java
@@ -2,12 +2,16 @@ package niriksha;
 
 public class Boulder extends Obstacle {
 	
+	boolean on_switch;
+	FloorSwitch f_s;
+	
 	enum action {
 		MOVE, DESTROYED, NOTHING;
 	}
 	
 	public Boulder(int x, int y) {
 		super(x, y, 'D');
+		this.on_switch = false;
 	}
 	
 	public action push_boulder(char direction, char type, Object object, int border) {
@@ -17,16 +21,28 @@ public class Boulder extends Obstacle {
 			case 'E':
 			case 'F':
 			case 'G':
+				if (this.on_switch == true) {
+					this.f_s.deactivateTrigger();
+					this.f_s = null;
+					this.on_switch = false;
+				}
 				return action.NOTHING;
 			
 			// floor switch
 			case 'I':
 				moveCoOrd(direction, border);
+				this.f_s = ((FloorSwitch) object);
 				((FloorSwitch) object).activateTrigger();
+				this.on_switch = true;
 				return action.MOVE;
 			
 			// pit
 			case 'B':
+				if (this.on_switch == true) {
+					this.f_s.deactivateTrigger();
+					this.f_s = null;
+					this.on_switch = false;
+				}
 				moveCoOrd(direction, border);
 				destroyBoulder(this);
 				return action.DESTROYED;
