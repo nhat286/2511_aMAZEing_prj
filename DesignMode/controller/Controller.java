53ba29ba15ab156173c05c630e3c847525cb555e
niriksha99 <30465025+niriksha99@users.noreply.github.com>
2018-10-20 21:18:28 +1100

    not completed

---
+++ DesignMode/controller/Controller.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Controller {
+	
+	private Stage stage;
+	
+	public Controller(Stage s) {
+		this.stage = s;
+	}
+	
+	public Stage getStage() {
+		return stage;
+	}
+	
+    /*@FXML
+    private void mouseEntered(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
+    }*/
+	
+	
+
+}
---
+++ DesignMode/controller/Main.java
@@ -0,0 +1,20 @@
+package controller;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		Screen cartScreen = new Screen(primaryStage, "Movie Basket", "view/basket.fxml");
+        Controller bc = new Test(primaryStage);
+        cartScreen.display(bc);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
